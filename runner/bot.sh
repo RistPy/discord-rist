8f14c83a7e2bd84a7135db0cd0cbdc732224f2b5
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-14 13:25:23 +0530

    Update bot.sh

--- runner/bot.sh
+++ runner/bot.sh
@@ -1,6 +1,6 @@
 # install requirements
 python3 -m pip install -r requirements.txt
 # compile cog file
-python3 -m ristpy src/cogs/say.rist --compile-to src/cogs/say.py
+ristpy src/cogs/say.rist --compile-to src/cogs/say.py
 # now run
 python3 -m ristpy src/bot.rist
